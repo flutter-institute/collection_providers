167a2c7db9603a7b3f5e5e3f58883f4924af8840
Brian Armstrong <brian@flutter.institute>
2020-08-05 22:51:50 -0600

    Updating CollectionConsumer to have the ctor arguments it needs to pass on

--- lib/src/collection_consumer.dart
+++ lib/src/collection_consumer.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/widgets.dart';
 import 'package:collection_providers/collection_providers.dart';
 import 'package:provider/provider.dart';
 
 class CollectionConsumer<T extends CollectionChangeNotifier>
     extends Consumer<T> {
-  // noop, just a wrapper for type enforcement
+  CollectionConsumer({
+    Key key,
+    @required
+        Widget Function(BuildContext context, T value, Widget child) builder,
+    Widget child,
+  })  : assert(builder != null),
+        super(key: key, builder: builder, child: child);
 }
